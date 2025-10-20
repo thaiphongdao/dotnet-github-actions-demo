7041ffb18008ceffd4b8164c95235d924a481492
Phong Dao <pdao@pilotgames.com>
2025-10-19 20:20:24 -0700

    Initial project setup

---
+++ DemoApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace DemoApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+    }
+}
